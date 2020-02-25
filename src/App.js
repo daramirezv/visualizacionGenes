b9615ac20fad1873133924e6c016bb52750f61f3
daramirezv <30914399+daramirezv@users.noreply.github.com>
2020-02-25 17:17:13 -0500

    separacion de componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 import './App.css';
+import AppFirstGraph from './AppFirstGraph';
+import AppSecondGraph from './AppSecondGraph';
 import cancer from './cancer.txt';
-const uuidv4 = require('uuid/v4');
 
 class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { mesActual: "", objetosSitios: [] };
-        // this.onMouseOver = this.onMouseOver.bind(this);
+        this.state = { datosPrimerGrafica: [], datosSegundaGrafica: [], nombresGenes: [] };
     }
 
-
-
     componentDidMount() {
-        //Arreglo donde se va a guardar el arreglo que usa D3 para la primera grafica
-        var resultadofinal = [];
 
-        //Función que calcula la entropia de shannon para la posición "x" de los genes.
-        //Usa la libreria uuidv4 para asegurarse que cada "-" se tome como diferente.
         let shannon = (array) => {
             if (array.length === 0)
                 return null;
@@ -28,10 +22,7 @@ class App extends Component {
 
             for (let i = 0; i < array.length; i++) {
                 let el = array[i];
-                if (el === "-") {
-                    modeMap[uuidv4()] = 1;
-                }
-                else if (modeMap[el] == null) {
+                if (modeMap[el] == null) {
                     modeMap[el] = 1;
                 }
                 else {
@@ -49,33 +40,31 @@ class App extends Component {
             return resultado;
         }
 
-        //Método que lee el archivo
         d3.text(cancer).then(data => {
-
-            //Esto se usa para partir la informacion inicial en 3 arreglos
+            let resultadoPrimeraGrafica = [];
+            let resultadoSegundaGrafica = [];
             let arreglosIniciales = data.split(">");
             let respuesta = [];
+            let nombres = [];
 
             for (let index = 0; index < arreglosIniciales.length; index++) {
                 if (index === 0) {
                     continue;
                 }
                 let myString = arreglosIniciales[index];
+                nombres.push(myString.substring(0, myString.indexOf('\n')))
                 myString = myString.substring(myString.indexOf('\n') + 1);
                 respuesta.push(myString);
             }
 
-
             let posPromedio = 0;
             let valPromedio = 0;
 
-            //El metodo que va a creando cada objeto de llave valor con la posicion y la entropia
-            for (let index = 0; index < respuesta[respuesta.length-1].length; index++) {
+            for (let index = 0; index < respuesta[respuesta.length - 1].length; index++) {
                 let correccionSalto = 0;
                 let componentes = []
 
                 for (let indexInterno = 0; indexInterno < respuesta.length; indexInterno++) {
-                    //console.log(componentes);
                     componentes.push(respuesta[indexInterno].charAt(index));
                 }
 
@@ -88,7 +77,7 @@ class App extends Component {
 
                 if (posPromedio >= 9 || index + 1 == respuesta[2].length) {
                     let objetoTemp = new myObject(index - correccionSalto, valPromedio / (posPromedio + 1));
-                    resultadofinal.push(objetoTemp);
+                    resultadoPrimeraGrafica.push(objetoTemp);
                     valPromedio = 0;
                     posPromedio = 0;
                 }
@@ -97,132 +86,43 @@ class App extends Component {
                 }
 
             }
-            //console.log(resultadofinal);
 
-            //D3 PRIMER GRAFICO
+            for (let index = 0; index < respuesta[respuesta.length - 1].length; index++) {
+                let componentes = [];
 
-            var svg = d3.select("svg"),
-                margin = { top: 20, right: 20, bottom: 110, left: 40 },
-                margin2 = { top: 430, right: 20, bottom: 30, left: 40 },
-                width = +svg.attr("width") - margin.left - margin.right,
-                height = +svg.attr("height") - margin.top - margin.bottom,
-                height2 = +svg.attr("height") - margin2.top - margin2.bottom;
+                for (let indexInterno = 0; indexInterno < respuesta.length; indexInterno++) {
+                    componentes.push(respuesta[indexInterno].charAt(index));
+                }
 
-            var x = d3.scaleLinear().range([0, width]),
-                x2 = d3.scaleLinear().range([0, width]),
-                y = d3.scaleLinear().range([height, 0]),
-                y2 = d3.scaleLinear().range([height2, 0]);
+                if (componentes.some(v => v !== "-" && v !== "A" && v !== "T" && v !== "C" && v !== "G")) {
+                    continue;
+                }
 
-            var xAxis = d3.axisBottom(x),
-                xAxis2 = d3.axisBottom(x2),
-                yAxis = d3.axisLeft(y);
+                let objetoTemp = "{ \"posicion\":" + (index + 1) + "";
 
-            var brush = d3.brushX()
-                .extent([[0, 0], [width, height2]])
-                .on("brush end", brushed);
+                for (let pos = 0; pos < componentes.length; pos++) {
 
-            var zoom = d3.zoom()
-                .scaleExtent([1, Infinity])
-                .translateExtent([[0, 0], [width, height]])
-                .extent([[0, 0], [width, height]])
-                .on("zoom", zoomed);
+                    objetoTemp += ", \"" + nombres[pos] + "\":\"" + respuesta[pos][index] + "\"";
+                }
 
-            var area = d3.area()
-                .curve(d3.curveMonotoneX)
-                .x(function (d) { return x(d.posicion); })
-                .y0(height)
-                .y1(function (d) { return y(d.porcentaje); });
-
-            var area2 = d3.area()
-                .curve(d3.curveMonotoneX)
-                .x(function (d) { return x2(d.posicion); })
-                .y0(height2)
-                .y1(function (d) { return y2(d.porcentaje); });
-
-            svg.append("defs").append("clipPath")
-                .attr("id", "clip")
-                .append("rect")
-                .attr("width", width)
-                .attr("height", height);
-
-            var focus = svg.append("g")
-                .attr("class", "focus")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-            var context = svg.append("g")
-                .attr("class", "context")
-                .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-
-            x.domain(d3.extent(resultadofinal, function (d) { return d.posicion; }));
-            y.domain([0, d3.max(resultadofinal, function (d) { return d.porcentaje; })]);
-            x2.domain(x.domain());
-            y2.domain(y.domain());
-
-            focus.append("path")
-                .datum(resultadofinal)
-                .attr("class", "area")
-                .attr("d", area);
-
-            focus.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            focus.append("g")
-                .attr("class", "axis axis--y")
-                .call(yAxis);
-
-            context.append("path")
-                .datum(resultadofinal)
-                .attr("class", "area")
-                .attr("d", area2);
-
-            context.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height2 + ")")
-                .call(xAxis2);
-
-            context.append("g")
-                .attr("class", "brush")
-                .call(brush)
-                .call(brush.move, x.range());
-
-            svg.append("rect")
-                .attr("class", "zoom")
-                .attr("width", width)
-                .attr("height", height)
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-                .call(zoom);
-
-            function brushed() {
-                if (d3.event.sourceEvent && d3.event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
-                var s = d3.event.selection || x2.range();
-                x.domain(s.map(x2.invert, x2));
-                focus.select(".area").attr("d", area);
-                focus.select(".axis--x").call(xAxis);
-                svg.select(".zoom").call(zoom.transform, d3.zoomIdentity
-                    .scale(width / (s[1] - s[0]))
-                    .translate(-s[0], 0));
+                objetoTemp += "}";
+                let res = JSON.parse(objetoTemp)
+                resultadoSegundaGrafica.push(res);
             }
 
-            function zoomed() {
-                if (d3.event.sourceEvent && d3.event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
-                var t = d3.event.transform;
-                x.domain(t.rescaleX(x2).domain());
-                focus.select(".area").attr("d", area);
-                focus.select(".axis--x").call(xAxis);
-                context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
-            }
+            this.setState({
+                datosPrimerGrafica: resultadoPrimeraGrafica,
+                datosSegundaGrafica: resultadoSegundaGrafica,
+                nombresGenes: nombres
+            })
         })
     }
-
 
     render() {
         return (
             <div className="App">
-                <svg width="960" height="500"></svg>
-                <div id="chart"></div>;
+                {/* {this.state.datosSegundaGrafica.length > 0 && this.state.nombresGenes.length > 0 ? <AppSecondGraph datosSegundaGrafica={this.state.datosSegundaGrafica} nombresGenes={this.state.nombresGenes} />: <h2>Loading...</h2>} */}
+                {this.state.datosPrimerGrafica.length > 0 ? <AppFirstGraph datosPrimerGrafica={this.state.datosPrimerGrafica} /> : <h2>Loading...</h2>}
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import AppC3 from './AppC3';
-import AppK2 from './AppK2';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppC3 />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
