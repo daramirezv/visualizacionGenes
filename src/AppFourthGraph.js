4070e16f9b59de8c5b4e88ba6091f636586bd1a9
daramirezv <30914399+daramirezv@users.noreply.github.com>
2020-03-04 17:50:57 -0500

    filtro tabla

--- src/AppFourthGraph.js
+++ src/AppFourthGraph.js
@@ -1,20 +1,38 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
 import './AppFourthGraph.css';
 
 class AppFourthGraph extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { boxesseleccionados: [] };
         this.posiciones = this.posiciones.bind(this);
         this.anidada = this.anidada.bind(this);
+        this.checkboxes = this.checkboxes.bind(this);
+        this.filter = this.filter.bind(this);
+        this.selectAll = this.selectAll.bind(this);
+        this.deselectAll = this.deselectAll.bind(this);
         this.tableref = React.createRef();
     }
 
+    componentDidMount() {
+        this.selectAll();
+    }
+
+    componentDidUpdate() {
+        d3.selectAll(".tabla-vertical")
+            .style("height", null);
+        if (d3.selectAll(".tabla-vertical").node().offsetHeight > 750) {
+            d3.selectAll(".tabla-vertical")
+                .style("height", "750px");
+        }
+    }
+
     posiciones() {
-        var primerValor = this.props.primerValor;
-        var segundoValor = this.props.segundoValor;
-        var datosSegundaGrafica = this.props.datosSegundaGrafica.slice(primerValor - 1, segundoValor);
+        let primerValor = this.props.primerValor;
+        let segundoValor = this.props.segundoValor;
+        let datosSegundaGrafica = this.props.datosSegundaGrafica.slice(primerValor - 1, segundoValor);
 
         return (
             datosSegundaGrafica.map(function (item, i) {
@@ -23,55 +41,109 @@ class AppFourthGraph extends Component {
     }
 
     valores(anidada) {
-        var nombres = this.props.nombresGenes;
+        let nombres = this.state.boxesseleccionados;
         return (
             nombres.map(function (item, i) {
                 return (
                     <tr key={i}>
-                        <th scope="row" key={i}>{item}</th>
+                        <td scope="row" className="stickyleft" key={i}>{item}</td>
                         {anidada(item)}
                     </tr>)
             }))
     }
 
     anidada(nombreGen) {
-        var primerValor = this.props.primerValor;
-        var segundoValor = this.props.segundoValor;
-        var datosSegundaGrafica = this.props.datosSegundaGrafica.slice(primerValor - 1, segundoValor);
+
+        let primerValor = this.props.primerValor;
+        let segundoValor = this.props.segundoValor;
+        let datosSegundaGrafica = this.props.datosSegundaGrafica.slice(primerValor - 1, segundoValor);
         return (
             datosSegundaGrafica.map(function (item, i) {
                 let caracter = datosSegundaGrafica[i][nombreGen.replace(/[\r\n]+/gm, "")];
                 let clase = "";
-                if(caracter === "-")
-                {
+                if (caracter === "-") {
                     clase = "gap"
                 }
-                else{
+                else {
                     clase = caracter;
                 }
                 return <td className={clase} key={i}><b>{caracter}</b></td>
             }))
     }
 
+    checkboxes() {
+        let nombres = this.props.nombresGenes;
+        return (
+            nombres.map(function (item, i) {
+                return (<div className="form-check espacioCheckbox" key={i}>
+                    <input className="form-check-input" type="checkbox" id={i} value={item} />
+                    <label className="form-check-label" htmlFor={i}>{item}</label>
+                </div>)
+            }))
+    }
+
+    filter() {
+        let arregloNombres = [];
+
+        let boxes = d3.selectAll(".form-check-input").nodes()
+        boxes.map(function (item, i) {
+            if (item.checked === true) {
+                arregloNombres.push(item.value);
+            }
+        })
+
+        this.setState({
+            boxesseleccionados: arregloNombres
+        });
+    }
+
+    selectAll() {
+        let boxes = d3.selectAll(".form-check-input").nodes()
+        boxes.map(function (item, i) {
+            item.checked = true;
+        })
+        this.filter();
+    }
+
+    deselectAll() {
+        let boxes = d3.selectAll(".form-check-input").nodes()
+        boxes.map(function (item, i) {
+            item.checked = false;
+        })
+        this.filter();
+    }
+
     render() {
         return (
-            <div id="my_dataviz">
+            <div className="cuerpo">
                 <div className="container">
                     <div className="row">
                         <div className="col-md">
-                            <div className ="table-responsive tabla-vertical">
-                            <table className="table table-striped table-bordered" ref={this.tableref}>
-                                <thead>
-                                    <tr>
-                                        <th className="stickyhead"scope="col">Name</th>
-                                        {this.posiciones()}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.valores(this.anidada)}
-                                </tbody>
-                            </table>
+                            <div className="table-responsive tabla-vertical" ref={this.tableref}>
+                                <table className="table table-striped table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th className="stickyhead" scope="col"></th>
+                                            {this.posiciones()}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.valores(this.anidada)}
+                                    </tbody>
+                                </table>
                             </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md">
+                            <button className="btn btn-primary" type="submit" onClick={this.filter}>Filter</button>
+                            <button className="btn btn-primary" type="submit" onClick={this.selectAll}>Select All Genes</button>
+                            <button className="btn btn-primary" type="submit" onClick={this.deselectAll}>Deselect All Genes</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md">
+                            {this.checkboxes()}
                         </div>
                     </div>
                 </div>
